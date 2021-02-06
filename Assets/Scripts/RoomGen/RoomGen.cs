3fe1481cd2b603b27beca07aeedc06cfb15143fe
EdwardDobson <47003895+EdwardDobson@users.noreply.github.com>
2021-02-06 08:40:03 +0000

    Update
    -Multiple rooms can now be spawned

--- Assets/Scripts/RoomGen/RoomGen.cs
+++ Assets/Scripts/RoomGen/RoomGen.cs
@@ -4,81 +4,113 @@ using UnityEngine;
 using UnityEngine.Tilemaps;
 public class RoomGen : MonoBehaviour
 {
-  [SerializeField]
-  Tilemap m_tilemap;
-  [SerializeField]
-  Vector2Int m_wallDimensions;
-  [SerializeField]
-  List<TileBase> m_tiles = new List<TileBase>();
-  [SerializeField]
-  List<Vector3Int> m_tilePostions = new List<Vector3Int>();
-  Vector3Int m_tempPosition;
-  int m_doorAmount;
+    [SerializeField]
+    Tilemap m_tilemap;
+    [SerializeField]
+    Vector2Int m_wallDimensions;
+    [SerializeField]
+    int m_wallMaxX;
+    [SerializeField]
+    int m_wallMinX;
+    [SerializeField]
+    int m_wallMaxY;
+    [SerializeField]
+    int m_wallMinY;
+    [SerializeField]
+    List<TileBase> m_tiles = new List<TileBase>();
+    [SerializeField]
+    List<Vector3Int> m_tilePostions = new List<Vector3Int>();
+    Vector3Int m_tempPosition;
+    [SerializeField]
+    Vector2Int m_dungeonDimensions;
+    int m_doorAmount;
+    [SerializeField]
+    List<Vector2Int> m_buildPoints = new List<Vector2Int>();
+    [SerializeField]
+    Vector2Int m_buildPointChoice;
 
     void Start()
     {
-      m_doorAmount = Random.Range(1, 4);
-      RebuildRoom();
+        for (int x = 0; x < m_dungeonDimensions.x; ++x)
+        {
+            for (int y = 0; y < m_dungeonDimensions.y; ++y)
+            {
+                Vector2Int temp = new Vector2Int(x, y);
+                m_buildPoints.Add(temp);
+            }
+        }
+        m_buildPointChoice = m_buildPoints[Random.Range(0, m_buildPoints.Count)];
+
+        RebuildRoom(1);
+
     }
     void BuildWalls()
     {
-      m_tilePostions.Clear();
-      for(int i = 0 ; i < m_wallDimensions.x + 1; ++i)
-      {
-        BuildPiece(i,0,0);
-        if(m_tempPosition != new Vector3Int(0,0,0) && m_tempPosition != new Vector3Int(m_wallDimensions.x,0,0) && m_tempPosition != new Vector3Int(m_wallDimensions.x,m_wallDimensions.y,0))
+        m_tilePostions.Clear();
+        for (int i = 0; i < m_wallDimensions.x + 1; ++i)
         {
-            m_tilePostions.Add(m_tempPosition);
+            BuildPiece(m_buildPointChoice.x + i, m_buildPointChoice.y, 0);
+            if (m_tempPosition != new Vector3Int(m_buildPointChoice.x, m_buildPointChoice.y, 0) && m_tempPosition != new Vector3Int(m_buildPointChoice.x, m_buildPointChoice.y + m_wallDimensions.y, 0) && m_tempPosition != new Vector3Int(m_buildPointChoice.x +m_wallDimensions.x, m_buildPointChoice.y, 0))
+                m_tilePostions.Add(m_tempPosition);
+
+            BuildPiece(m_buildPointChoice.x + i, m_buildPointChoice.y + m_wallDimensions.y, 0);
+            m_buildPoints.Remove((Vector2Int)m_tempPosition);
         }
-        BuildPiece(i,m_wallDimensions.y,0);
-      }
-      for(int a = 0 ; a < m_wallDimensions.y; ++a)
-      {
-        BuildPiece(0,a,0);
-        if(m_tempPosition != new Vector3Int(0,0,0)&& m_tempPosition != new Vector3Int(0,m_wallDimensions.y,0)&& m_tempPosition != new Vector3Int(m_wallDimensions.x,m_wallDimensions.y,0))
+        for (int a = 0; a < m_wallDimensions.y + 1; ++a)
         {
-            m_tilePostions.Add(m_tempPosition);
+            BuildPiece(m_buildPointChoice.x, m_buildPointChoice.y + a, 0);
+            if (m_tempPosition != new Vector3Int(m_buildPointChoice.x, m_buildPointChoice.y, 0) && m_tempPosition != new Vector3Int(m_buildPointChoice.x, m_buildPointChoice.y + m_wallDimensions.y, 0) && m_tempPosition != new Vector3Int(m_buildPointChoice.x + m_wallDimensions.x, m_buildPointChoice.y, 0))
+                m_tilePostions.Add(m_tempPosition);
+
+            BuildPiece(m_buildPointChoice.x + m_wallDimensions.x, m_buildPointChoice.y + a, 0);
+            m_buildPoints.Remove((Vector2Int)m_tempPosition);
         }
-        BuildPiece(m_wallDimensions.x,a,0);
-      }
-      for(int i  =0 ; i < m_doorAmount; ++i)
-      {
-        PlaceDoor();
-      }
-      FillFloor();
+        for (int i = 0; i < m_doorAmount; ++i)
+        {
+            PlaceDoor();
+        }
+        FillFloor();
     }
     void BuildPiece(int _value1, int _value2, int _tileIndex)
     {
-      Vector3Int posY = new Vector3Int(_value1,_value2,0);
-      if(m_tilemap.GetTile(posY) == null)
-      m_tilemap.SetTile(posY,m_tiles[_tileIndex]);
-      m_tempPosition = posY;
+        Vector3Int posY = new Vector3Int(_value1, _value2, 0);
+        if (m_tilemap.GetTile(posY) == null)
+            m_tilemap.SetTile(posY, m_tiles[_tileIndex]);
+        m_tempPosition = posY;
 
     }
     void FillFloor()
     {
-      for(int i = 0 ; i < m_wallDimensions.x; ++i)
-      {
-          for(int a = 0 ; a < m_wallDimensions.y; ++a)
-          {
-            BuildPiece(i,a,1);
-          }
-      }
+        for (int i = 0; i < m_wallDimensions.x; ++i)
+        {
+            for (int a = 0; a < m_wallDimensions.y; ++a)
+            {
+                BuildPiece(m_buildPointChoice.x + i, m_buildPointChoice.y + a, 1);
+            }
+        }
     }
-    public void RebuildRoom()
+    public void RebuildRoom(int _times)
     {
-      RandomiseWallDimensions();
-      BuildWalls();
+        for(int i = 0; i < _times; ++i)
+        {
+            m_doorAmount = Random.Range(1, 4);
+            m_buildPointChoice = m_buildPoints[Random.Range(0, m_buildPoints.Count)];
+            RandomiseWallDimensions();
+            BuildWalls();
+        }
+    }
+    public void ClearTileMap()
+    {
+        m_tilemap.ClearAllTiles();
     }
     void PlaceDoor()
     {
-      int randomPosChoice = Random.Range(0, m_tilePostions.Count);
-      Vector3Int randomWallPos = m_tilePostions[randomPosChoice];
-      m_tilemap.SetTile(randomWallPos, null);
-      Debug.Log(randomWallPos);
+        int randomPosChoice = Random.Range(0, m_tilePostions.Count);
+        Vector3Int randomWallPos = m_tilePostions[randomPosChoice];
+        m_tilemap.SetTile(randomWallPos, null);
     }
     void RandomiseWallDimensions()
     {
-      m_wallDimensions = new Vector2Int(Random.Range(5, 25),Random.Range(5, 25));
+        m_wallDimensions = new Vector2Int(Random.Range(m_wallMinX, m_wallMaxX), Random.Range(m_wallMinY, m_wallMaxY));
     }
 }
